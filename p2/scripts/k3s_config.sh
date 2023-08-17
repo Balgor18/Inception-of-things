f1da4e1ca16f8dc3605c9ec4d3aef9668815fbc9
Maxime <victorportenseigne@gmail.com>
2023-08-17 11:17:23 +0200

    pass to centos/8

--- p2/scripts/k3s_config.sh
+++ p2/scripts/k3s_config.sh
@@ -3,27 +3,38 @@
 echo "[INFO]  GO TO INSTALL k3s (ip: $1)"
 
 echo "[INFO] config var k3s"
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 "
-echo "[INFO]  ARGUMENT PASSED TO INSTALL_K3S_EXEC: $INSTALL_K3S_EXEC"
+cd /etc/yum.repos.d/
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface eth1" sh -
 
-echo "[INFO] add dep"
-apk add curl
-
-echo "[INFO] dep install - go install k3s"
-curl -sfL https://get.k3s.io |  sh -
 echo "[INFO]  Successfully installed k3s on server node"
-
-echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh # way to add alias on all users
+echo "[INFO]  END"
+sleep 10
+/usr/local/bin/k3s kubectl get nodes
+echo "[INFO]  END TEST"
+sleep 10
+/usr/local/bin/k3s kubectl get jobs -n kube-system 
+/usr/local/bin/k3s kubectl -n kube-system wait --for=condition=complete --timeout=-1s jobs/helm-install-traefik-crd
+/usr/local/bin/k3s kubectl -n kube-system wait --for=condition=complete --timeout=-1s jobs/helm-install-traefik
+/usr/local/bin/k3s kubectl get jobs -n kube-system
 
 echo "[INFO]  Doing some sleep to wait for k3s to be ready"
 sleep 10
 
-kubectl apply -f /vagrant/scripts/services.yml; sleep 2
+echo "[INFO]  STARTING APP DEPLOYMENT"
+/usr/local/bin/k3s kubectl apply -f /vagrant/scripts/service.yml; sleep 2
+echo "[INFO]  STARTING APP DEPLOYMENT"
 
-kubectl apply -f /vagrant/scripts/app1.yml; sleep 2
+/usr/local/bin/k3s kubectl apply -f /vagrant/scripts/app1.yml; sleep 2
+echo "[INFO]  STARTING APP DEPLOYMENT"
 
-kubectl apply -f /vagrant/scripts/app2.yml; sleep 2
+/usr/local/bin/k3s kubectl apply -f /vagrant/scripts/app2.yml; sleep 2
+echo "[INFO]  STARTING APP DEPLOYMENT"
 
-kubectl apply -f /vagrant/scripts/app3.yml; sleep 2
+/usr/local/bin/k3s kubectl apply -f /vagrant/scripts/app3.yml; sleep 2
+echo "[INFO]  STARTING APP DEPLOYMENT"
 
-kubectl apply -f /vagrant/scripts/ingress.yml; sleep 2
+/usr/local/bin/k3s kubectl apply -f /vagrant/scripts/ingress.yml; sleep 2
+echo "[INFO]  END"
+
