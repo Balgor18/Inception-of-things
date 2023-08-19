28f35c7edf471749d68564ef330e7f8d2be798a2
Florian Catinaud <catinaud.florian@gmail.com>
2023-08-19 18:38:26 +0200

    Fix the problem with ArgoCD.

--- p3/install/setup.sh
+++ p3/install/setup.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 
 echo "###################### Creation Cluster ######################"
 #  Create the cluster
-k3d cluster create ArgonCD 1> /dev/null
+k3d cluster create argocd
 
 echo "###################### Creation Namespace ######################"
 # Create both namespace
@@ -10,20 +11,16 @@ kubectl create namespace argocd
 
 echo "###################### Install ArgoCD ######################"
 # Install argocd on pods
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.3.5/manifests/install.yaml
 
 echo "###################### Deploy script ######################"
 # Need to apply the script of deployement
 
 # Boucle while to wait pod ready
-# while [[ $(kubectl get pods -n argocd -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True True True True True True True" ]]; \
-#  do echo "[INFO][ARGOCD]  Waiting all pods to be ready..." && sleep 10; done
-#  Erreur que ca donne
-#  fcatinauS: E0818 20:07:18.973462    6437 memcache.go:287] couldn't get resource list for metrics.k8s.io/v1beta1: the server is currently unable to handle the request
-#    fcatinauS: E0818 20:07:19.025224    6437 memcache.go:121] couldn't get resource list for metrics.k8s.io/v1beta1: the server is currently unable to handle the request
-#    fcatinauS: E0818 20:07:19.042239    6437 memcache.go:121] couldn't get resource list for metrics.k8s.io/v1beta1: the server is currently unable to handle the request
-#    fcatinauS: E0818 20:07:19.048489    6437 memcache.go:121] couldn't get resource list for metrics.k8s.io/v1beta1: the server is currently unable to handle the request
-echo "[INFO][ARGOCD] Done"
+while [[ $(kubectl get pods -n argocd -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' 2> /dev/null) != "True True True True True True True" ]]; \
+ do echo "Waiting all pods to be ready..." && sleep 10; done
+
+echo "Done"
 
 # k apply -f application.yaml
 
