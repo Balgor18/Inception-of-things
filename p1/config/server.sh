48792f3bd691fa2caf200a1335a178150bf19e8a
Florian Catinaud <catinaud.florian@gmail.com>
2023-08-20 20:29:59 +0200

    Clear file for no useless file

--- p1/config/server.sh
+++ p1/config/server.sh
@@ -1,5 +1,6 @@
-apk add curl 
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 "
+
+apk add curl 
 curl -sfL https://get.k3s.io |  sh - # Litteraly from the documentation
 
 sleep 10 # Wait and of installation
