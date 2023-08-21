fa651d36323496e82baa80f207db881b220fbe23
viporten <viporten@student.42.fr>
2023-08-21 17:22:44 +0200

    start install helm

--- bonus/install/helm.sh
+++ bonus/install/helm.sh
@@ -1,1 +1,13 @@
-## ttps://docs.gitlab.com/charts/
+## https://docs.gitlab.com/charts/
+
+# Prerequit
+snap install helm --classic
+
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
+# helm upgrade --install gitlab gitlab/gitlab \
+#   --timeout 600s \
+#   --set global.hosts.domain=$HOSTNAME.gitlab.com \
+#   --set global.hosts.externalIP=192.168.56.110 \
+#   --set certmanager-issuer.email=test@test.com \
+  # --set postgresql.image.tag=13.6.0
