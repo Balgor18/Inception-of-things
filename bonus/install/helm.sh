b7e0bc3447b24a7fc8b93573b5c25d69da1b6bc5
Florian Catinaud <catinaud.florian@gmail.com>
2023-08-21 21:12:43 +0200

    Start gitlab to the repo but error on the launch of pods of gitlab

--- bonus/install/helm.sh
+++ bonus/install/helm.sh
@@ -1,13 +1,30 @@
-## https://docs.gitlab.com/charts/
+#!/bin/bash
 
-# Prerequit
-snap install helm --classic
+echo "###################### Install Helm ######################"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 
-helm repo add gitlab https://charts.gitlab.io/
-helm repo update
-# helm upgrade --install gitlab gitlab/gitlab \
-#   --timeout 600s \
-#   --set global.hosts.domain=$HOSTNAME.gitlab.com \
-#   --set global.hosts.externalIP=192.168.56.110 \
-#   --set certmanager-issuer.email=test@test.com \
-  # --set postgresql.image.tag=13.6.0
+echo "###################### Install postgrel ######################"
+sudo helm install my-release oci://registry-1.docker.io/bitnamicharts/postgresql
+
+echo "###################### Install Gitlab ######################"
+kubectl create namespace gitlab
+sudo helm repo add gitlab https://charts.gitlab.io/
+sudo helm search repo gitlab
+sudo helm repo update
+
+sudo helm upgrade --install gitlab gitlab/gitlab \
+  --timeout 600s \
+  --set global.hosts.domain=fcatinau.com \
+  --set global.hosts.externalIP=192.168.56.110 \
+  --set certmanager-issuer.email=fcatinau@student.42.fr \
+  --set global.hosts.https="false"\
+  --set gitlab-runner.install="false" -n gitlab
+
+echo -n "Gitlab password: "
+
+kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath='{.data.password}' | base64 -d; echo
+
+echo "Wait for deployement of GitLab"
+kubectl wait -n gitlab --for=condition=available deployment --all --timeout=-1s
