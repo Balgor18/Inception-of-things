f44511ec8807395ac31d29722f375ad83f785ab5
Florian Catinaud <catinaud.florian@gmail.com>
2023-08-30 15:44:05 +0200

    WIP the bonus part. Error on gitlab

--- bonus/install/helm.sh
+++ bonus/install/helm.sh
@@ -11,20 +11,26 @@ chmod 700 get_helm.sh
 echo "###################### Install Gitlab ######################"
 sudo kubectl create namespace gitlab
 sudo helm repo add gitlab https://charts.gitlab.io/
-sudo helm search repo gitlab
+# sudo helm search repo gitlab
 sudo helm repo update
+sudo helm show values gitlab/gitlab > gitlab/values.yaml
 
 sudo helm upgrade --install gitlab gitlab/gitlab \
   --timeout 600s \
   --set global.hosts.domain=gitlab.fcatinau.com \
   --set certmanager-issuer.email=fcatinau@student.42.fr \
   --set global.ingress.configureCertmanager="false"\
+  --set global.hosts.externalIP=127.0.0.1 \
   --set global.hosts.https="false"\
   --set gitlab-runner.install="false" -n gitlab
 
-echo -n "Gitlab password: "
+  # --set global.edition=ce \
+  # --set certmanager-issuer.email=me@example.com \
 
-sudo kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath='{.data.password}' | base64 -d; echo
+# echo -n "Gitlab password: "
 
-echo "Wait for deployement of GitLab"
-sudo kubectl wait -n gitlab --for=condition=available deployment --all --timeout=-1s
+# sudo kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath='{.data.password}' | base64 -d; echo
+
+echo "###################### Done ######################"
+# echo "Wait for deployement of GitLab"
+# sudo kubectl wait -n gitlab --for=condition=available deployment --all --timeout=-1s
